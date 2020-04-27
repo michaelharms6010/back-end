a6b5fa173b59de8e0b2590bb51717b685ab99161
Dylan Collins <dcollins5590@students.mc3.edu>
2020-04-27 14:09:37 -0400

    finished with all the seeds and table migrations

---
+++ migrations/20200427122928_website_tables.js
@@ -0,0 +1,44 @@
+
+exports.up = function(knex) {
+  return knex.schema
+    .createTable('users', tbl =>{
+        tbl.increments('id')
+        tbl.string('username', 40).unique().notNullable()
+        tbl.string('password', 40).notNullable()
+        tbl.string('email', 60).unique().notNullable()
+        tbl.string('name', 100).notNullable()
+        tbl.integer('age').notNullable()
+        tbl.boolean('terms').defaultTo(false); 
+    })
+    .createTable('posts', tbl =>{
+        tbl.increments('id')
+        tbl.blob('post').notNullable()
+        tbl.string('caption', 250)
+        tbl.date('date')
+        tbl.integer('user_id').notNullable().references('id').inTable('users');
+    })
+    .createTable('comments', tbl =>{
+        tbl.increments('id')
+        tbl.string('comment', 250).notNullable()
+        tbl.date('date');
+    })
+    .createTable('posts_comments', tbl =>{
+        tbl.increments('id');
+        tbl.integer('posts_id').notNullable().references('id').inTable('posts');
+        tbl.integer('comments_id').notNullable().references('id').inTable('comments');
+    })
+    .createTable('users_comments', tbl =>{
+        tbl.increments('id')
+        tbl.integer('users_id').notNullable().references('id').inTable('users');
+        tbl.integer('comments_id').notNullable().references('id').inTable('comments');
+    })
+};
+
+exports.down = function(knex) {
+  return knex.schema
+  .dropTableIfExists('users_comments')
+  .dropTableIfExists('posts_comments')
+  .dropTableIfExists('comments')
+  .dropTableIfExists('posts')
+  .dropTableIfExists('users');
+};
