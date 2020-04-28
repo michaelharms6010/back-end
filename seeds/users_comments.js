c9731dc992fa1125b7c66ae0bd72209694f6cf41
Dylan Collins <dcollins5590@students.mc3.edu>
2020-04-28 19:44:03 -0400

    handled

--- seeds/users_comments.js
+++ seeds/users_comments.js
@@ -2,7 +2,6 @@ exports.seed = function (knex) {
   return knex("users_comments")
     .del()
     .then(function () {
-<<<<<<< HEAD
       return knex('users_comments').insert([
         { usersId: 7, commentsId: 1},
         { usersId: 7, commentsId: 2},
@@ -22,27 +21,6 @@ exports.seed = function (knex) {
         { usersId: 2, commentsId: 16},
         { usersId: 1, commentsId: 17},
         { usersId: 1, commentsId: 18}
-=======
-      return knex("users_comments").insert([
-        { users_id: 7, comments_id: 1 },
-        { users_id: 7, comments_id: 2 },
-        { users_id: 6, comments_id: 3 },
-        { users_id: 6, comments_id: 4 },
-        { users_id: 6, comments_id: 5 },
-        { users_id: 5, comments_id: 6 },
-        { users_id: 5, comments_id: 7 },
-        { users_id: 5, comments_id: 8 },
-        { users_id: 4, comments_id: 9 },
-        { users_id: 4, comments_id: 10 },
-        { users_id: 4, comments_id: 11 },
-        { users_id: 3, comments_id: 12 },
-        { users_id: 3, comments_id: 13 },
-        { users_id: 3, comments_id: 14 },
-        { users_id: 2, comments_id: 15 },
-        { users_id: 2, comments_id: 16 },
-        { users_id: 1, comments_id: 17 },
-        { users_id: 1, comments_id: 18 },
->>>>>>> 22c73dbbf656ba5a260427f8903495f08f8fb13c
       ]);
     });
 };
