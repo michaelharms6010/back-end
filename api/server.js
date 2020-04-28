a34781cd324f8d4ecabed48ec2626cad08176a2d
Alex Martinez <alexmartinez0071234@gmail.com>
2020-04-28 11:46:49 -0400

    deleted users endpoints and file

--- api/server.js
+++ api/server.js
@@ -1,18 +1,16 @@
 const express = require("express");
 const server = express();
 const authRouter = require("../auth/auth-router.js");
-const usersRouter = require("../users/users-router.js");
 const postRouter = require("../posts/posts-router.js");
 const commentRouter = require("../comments/comments-Router.js");
 const helmet = require("helmet");
 const cors = require("cors");
-const authorizationMiddleware = require('../auth/auth.js');//put in as middleware for the users/posts/comments endpoints before deploying and make sure it work correctly
+const authorizationMiddleware = require("../auth/auth.js"); //put in as middleware for the users/posts/comments endpoints before deploying and make sure it work correctly
 
 server.use(helmet());
 server.use(cors());
 server.use(express.json());
 server.use("/api/auth", authRouter);
-server.use("/api/users", usersRouter);
 server.use("/api/posts", postRouter);
 server.use("/api/comments", commentRouter);
 
