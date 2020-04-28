33de5d5a09ef9762149c7475a7a505a41be3bded
Dylan Collins <dcollins5590@students.mc3.edu>
2020-04-28 18:55:34 -0400

    cahnge something

--- migrations/20200427122928_website_tables.js
+++ migrations/20200427122928_website_tables.js
@@ -1,7 +1,7 @@
 exports.up = function (knex) {
   return knex.schema
     .createTable("users", (tbl) => {
-      tbl.increments("id");
+      tbl.increments("userId").primary();
       tbl.string("username", 40).unique().notNullable();
       tbl.string("password", 40).notNullable();
       tbl.string("email", 60).unique().notNullable();
@@ -10,33 +10,33 @@ exports.up = function (knex) {
       tbl.boolean("terms").defaultTo(false);
     })
     .createTable("posts", (tbl) => {
-      tbl.increments("id");
+      tbl.increments("postId").primary();
       tbl.string("post").notNullable();
       tbl.string("caption", 250);
       tbl.timestamp("date").defaultTo(knex.fn.now());
       tbl.integer("user_id")
-      .references("users.id")
+      .references("users.userId")
       .onDelete('RESTRICT')
       .onUpdate('CASCADE');
     })
     .createTable("comments", (tbl) => {
-      tbl.increments("id");
+      tbl.increments("commentId").primary();
       tbl.string("comment", 250).notNullable();
       tbl.timestamp("date").defaultTo(knex.fn.now());
       tbl.integer("post_id")
-      .references("posts.id")
+      .references("posts.postId")
       .onDelete('RESTRICT')
       .onUpdate('CASCADE');
     })
     .createTable("users_comments", (tbl) => {
-      tbl.increments("id");
+      tbl.increments("userCommentId").primary();
       tbl.integer("users_id")
-      .references("users.id")
+      .references("users.userId")
       .onDelete('RESTRICT')
       .onUpdate('CASCADE');
       tbl
         .integer("comments_id")
-        .references("comments.id")
+        .references("comments.commentId")
         .onDelete('RESTRICT')
         .onUpdate('CASCADE');
     });
