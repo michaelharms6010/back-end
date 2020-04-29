c56952adeadda0c1c8e3291a516b201d4f5e2d4e
Alex Martinez <alexmartinez0071234@gmail.com>
2020-04-29 13:46:29 -0400

    more testing

--- posts/posts.spec.js
+++ posts/posts.spec.js
@@ -50,7 +50,7 @@ describe("logginng in to do test", () => {
 describe("testing getting posts", () => {
   test("should return posts", async () => {
     await request(server)
-      .get("/api/posts")
+      .se.get("/api/posts")
       .then((res) => {
         expect(res.status).toBe(4000);
       });
